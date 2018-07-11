56696ae5cff3d04095bd9625788ba095c183598c
Yonatan Kreiner <yonatankreiner@gmail.com>
2018-07-11 10:10:45 +0300

    change mkdir

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ output_path = '/home/yonatan/Desktop/pictures/frames'
 
 def split_frames(video, output_path):
     if not os.path.isdir(output_path):
-        os.mkdir(output_path)
+        os.makedirs(output_path)
     
     cmd = 'ffmpeg -i ' + video + ' -r 1/1 ' + output_path + '/frame%03d.bmp'
     os.system(cmd)
