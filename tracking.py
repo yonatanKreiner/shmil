2c066e2cd9b388449b77a0b7db17fb234d3607a9
ofirelarat <96ofir11@gmail.com>
2018-07-12 02:05:14 +0300

    add colored line

--- tracking.py
+++ tracking.py
@@ -1,7 +1,6 @@
 import cv2
 import sys
-import draw_line
-
+from random import randint
 
 #bbox = cv2.selectROI(frame, False)
 
@@ -20,20 +19,22 @@ def getTrackers(trackerType):
         trackers = [cv2.TrackerGOTURN_create() for box in boxs]
     return trackers
 
-def drawRoute(route, frame):
+def drawRoute(route, frame, color):
     for i in range(len(route) - 1):
-        cv2.line(frame, (route[i][0], route[i][1]), (route[i+1][0], route[i+1][1]), (255,0,0), 3) 
+        cv2.line(frame, (route[i][0], route[i][1]), (route[i+1][0], route[i+1][1]), color, 2) 
 
-VIDEO_PATH = '/home/yonatan/Desktop/pictures/DJI_0002.MP4'
-
+VIDEO_PATH = 'videos/DJI_0002.MP4'
 
 tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
 tracker_type = tracker_types[2]
 
 # set up boxs
-boxs = [(120, 117, 20, 10), (45, 640, 20, 10), (405,410,20,10)]
+boxs = [(125, 120, 10, 20), (45, 640, 20, 10), (410,415,20,10), (1075,240,10,20), (300,320,10,20), (843,465,10,20),
+        (840,487,20,15), (820,475,15,15), (935,505,20,10), (978,515,20,10), (1055,520,15,15), (1020,550,10,15)]
+
 trackers = getTrackers(tracker_type)
 cars_count = len(boxs)
+colors = [(randint(0, 255),randint(0,255),randint(0, 255)) for i in range(cars_count)]
 routes = [[] for i in range(cars_count)]
 
 #video
@@ -43,6 +44,8 @@ if not video.isOpened():
     sys.exit()
 
 ok, frame = video.read()
+prev_frame = frame
+
 if not ok:
     print('Cannot read video file')
     sys.exit()
@@ -66,12 +69,13 @@ while True:
             bbox = boxs[i]
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+            #cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
             routes[i].append(p1)
         else :
             cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
         if (len(routes[i]) > 0):
-            drawRoute(routes[i], frame)
+            drawRoute(routes[i], frame, colors[i])
 
     cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
  
