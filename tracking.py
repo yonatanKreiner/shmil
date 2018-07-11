b3af8f94613003f3c32ce390e1b111c1696b926b
Yonatan Kreiner <yonatankreiner@gmail.com>
2018-07-12 02:14:38 +0300

    add thresh and mask

--- tracking.py
+++ tracking.py
@@ -1,9 +1,7 @@
 import cv2
 import sys
 import draw_line
-
-
-#bbox = cv2.selectROI(frame, False)
+import numpy
 
 def getTrackers(trackerType):
     if tracker_type == 'BOOSTING':
@@ -24,60 +22,62 @@ def drawRoute(route, frame):
     for i in range(len(route) - 1):
         cv2.line(frame, (route[i][0], route[i][1]), (route[i+1][0], route[i+1][1]), (255,0,0), 3) 
 
-VIDEO_PATH = '/home/yonatan/Desktop/pictures/DJI_0002.MP4'
-
+VIDEO_PATH = '/home/yonatan/Desktop/pictures/stab.mp4'
 
 tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
 tracker_type = tracker_types[2]
 
 # set up boxs
-boxs = [(120, 117, 20, 10), (45, 640, 20, 10), (405,410,20,10)]
+boxs = [(120, 117, 10, 20), (45, 640, 20, 10), (410,410,20,20)]
 trackers = getTrackers(tracker_type)
 cars_count = len(boxs)
 routes = [[] for i in range(cars_count)]
 
-#video
 video = cv2.VideoCapture(VIDEO_PATH)
 if not video.isOpened():
     print("Could not open video")
     sys.exit()
 
-ok, frame = video.read()
+ok, prev_frame = video.read()
 if not ok:
     print('Cannot read video file')
     sys.exit()
 
-oks = [trackers[i].init(frame, boxs[i]) for i in range(cars_count)]
+oks = [trackers[i].init(prev_frame, boxs[i]) for i in range(cars_count)]
 
 while True:
     ok, frame = video.read()
+    ok, frame2 = video.read()
+    ok, frame3 = video.read()
+    ok, frame4 = video.read()
     if not ok:
         break
-     
-    timer = cv2.getTickCount()
 
     for i in range(cars_count):
         oks[i], boxs[i] = trackers[i].update(frame)
 
-    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+    delta = cv2.absdiff(frame, prev_frame)
+    kernel = numpy.ones((5, 5), numpy.uint8)
+    kernel[2][2] = -24
+    masked = cv2.dilate(delta, kernel, iterations = 1)
+    thresh = cv2.threshold(masked, 100, 255, cv2.THRESH_BINARY)[1]
 
     for i in range(cars_count):
         if oks[i]:
             bbox = boxs[i]
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
             routes[i].append(p1)
-        else :
-            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+        # else :
+            # cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
         if (len(routes[i]) > 0):
-            drawRoute(routes[i], frame)
+            pass
+            #drawRoute(routes[i], frame)
 
-    cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
- 
-    cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
-
-    cv2.imshow("Tracking", frame)
+    cv2.imshow("Tracking", thresh)
 
     k = cv2.waitKey(1) & 0xff
     if k == 27 : break
+
+    prev_frame = frame
+    #cv2.waitKey()
