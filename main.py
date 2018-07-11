db2002ccfecf6346bf63730d15a4cc70b0ff9880
Yonatan Kreiner <yonatankreiner@gmail.com>
2018-07-11 10:06:10 +0300

    split video to frames

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+video = '/home/yonatan/Desktop/pictures/DJI_0002.MP4'
+output_path = '/home/yonatan/Desktop/pictures/frames'
+
+
+def split_frames(video, output_path):
+    if not os.path.isdir(output_path):
+        os.mkdir(output_path)
+    
+    cmd = 'ffmpeg -i ' + video + ' -r 1/1 ' + output_path + '/frame%03d.bmp'
+    os.system(cmd)
+
+split_frames(video, output_path)
