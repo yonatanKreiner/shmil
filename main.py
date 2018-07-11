730be83d47bbc3a74a33d5de8317be7d3d641c4a
neriag <neriag@gmail.com>
2018-07-11 10:37:37 +0300

    move split frames'

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import os
+from split_frames import split_frames
 
 video = '/home/yonatan/Desktop/pictures/DJI_0002.MP4'
 output_path = '/home/yonatan/Desktop/pictures/frames'
-
-
-def split_frames(video, output_path):
-    if not os.path.isdir(output_path):
-        os.makedirs(output_path)
-    
-    cmd = 'ffmpeg -i ' + video + ' -r 1/1 ' + output_path + '/frame%03d.bmp'
-    os.system(cmd)
 
 split_frames(video, output_path)
