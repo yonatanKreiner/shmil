fcc6dd5c14c1d340f3c09943c9582d2082f8236b
ofirelarat <96ofir11@gmail.com>
2018-07-11 11:17:10 +0300

    use tuple instead of class for point

--- draw-line.py
+++ draw-line.py
@@ -1,17 +1,13 @@
 import os
+import math
 import numpy as np
 import cv2
 
 FRAMES_OUTPUT_DIR = '../output_frames'
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
 def drawLine(image_path, first_point, second_point, save):
     img  = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    cv2.line(img, (first_point.x, first_point.y), (second_point.x, second_point.y), (255,0,0), 3)
+    cv2.line(img, (first_point[0], first_point[1]), (second_point[0], second_point[1]), (255,0,0), 3)
 
     image_path_splited = image_path.split('/')
     output_file_name = 'output ' + image_path_splited [len(image_path_splited) - 1]
